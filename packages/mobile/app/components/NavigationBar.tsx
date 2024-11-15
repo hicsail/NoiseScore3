fed6fcc17a5382b72b77e78400a52326782f6e5d
cbolles <collinbolles@gmail.com>
2024-11-15 12:33:40 -0500

    Basic navigation support

--- packages/mobile/app/components/NavigationBar.tsx
+++ packages/mobile/app/components/NavigationBar.tsx
@@ -2,28 +2,24 @@ import { useState } from 'react';
 import { Text, BottomNavigation } from 'react-native-paper';
 import { TouchableRipple } from 'react-native-paper';
 
-const MusicRoute = () => <Text>Music</Text>;
+const MapRoute = () => <Text>Map</Text>;
 
-const AlbumsRoute = () => <Text>Albums</Text>;
+const MeasureRoute = () => <Text>Measure</Text>;
 
-const RecentsRoute = () => <Text>Recents</Text>;
-
-const NotificationsRoute = () => <Text>Notifications</Text>;
+const AccountRoute = () => <Text>Account</Text>;
 
 export default function NavigationBar() {
   const [index, setIndex] = useState(0);
   const [routes] = useState([
-    { key: 'music', title: 'Favorites', focusedIcon: 'heart', unfocusedIcon: 'heart-outline'},
-    { key: 'albums', title: 'Albums', focusedIcon: 'album' },
-    { key: 'recents', title: 'Recents', focusedIcon: 'history' },
-    { key: 'notifications', title: 'Notifications', focusedIcon: 'bell', unfocusedIcon: 'bell-outline' },
+    { key: 'map', title: 'Map', focusedIcon: 'map', unfocusedIcon: 'map-outline'},
+    { key: 'measure', title: 'Measure', focusedIcon: 'microphone', unfocusedIcon: 'microphone-outline' },
+    { key: 'account', title: 'Account', focusedIcon: 'account-circle', unfocusedIcon: 'account-circle-outline' },
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
-    music: MusicRoute,
-    albums: AlbumsRoute,
-    recents: RecentsRoute,
-    notifications: NotificationsRoute,
+    map: MapRoute,
+    measure: MeasureRoute,
+    account: AccountRoute,
   });
 
   return (
@@ -32,6 +28,7 @@ export default function NavigationBar() {
         navigationState={{ index, routes }}
         onIndexChange={setIndex}
         renderScene={renderScene}
+        renderTouchable={({key, ...props}) => (<TouchableRipple key={key} {...props} />)}
       />
     </>
   );
