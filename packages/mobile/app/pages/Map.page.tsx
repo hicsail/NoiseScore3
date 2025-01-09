568088c4775d57154c568b7c2dfd40e4cb1c0b77
cbolles <collinbolles@gmail.com>
2025-01-09 16:04:27 -0500

    Shrinking dots

--- packages/mobile/app/pages/Map.page.tsx
+++ packages/mobile/app/pages/Map.page.tsx
@@ -31,6 +31,8 @@ export const generateTestData = (latMin: number, latMax: number, longMax: number
         coordinates: [longitude, latitude]
       }
     });
+
+    console.log(reading);
   }
 
   return {
@@ -45,150 +47,17 @@ export default function Map() {
       <View style={{ height: '100%', width: '100%' }}>
         <MapView style={{ flex: 1 }}>
           <Camera zoomLevel={10} centerCoordinate={[-71.07579904594763, 42.3412156414843]} />
-          <ShapeSource id='sound-heat' shape={generateTestData(LAT_MIN, LAT_MAX, LONG_MIN, LONG_MAX, 2000)}>
+          <ShapeSource id='sound-heat' shape={generateTestData(LAT_MIN, LAT_MAX, LONG_MIN, LONG_MAX, 10)}>
             <HeatmapLayer
               id='sound-heat'
               style={{
-                heatmapWeight: ['interpolate', ['linear'], ['get', 'db'], 0, 0, 6, 1]
+                heatmapWeight: ['interpolate', ['linear'], ['get', 'db'], 0, 60, 0, 130],
+                heatmapIntensity: ['interpolate', ['linear'], ['zoom'], 0, 1, 9, 3],
+                heatmapRadius: ['interpolate', ['linear'], ['zoom'], 0, 2, 9, 20]
               }} />
           </ShapeSource>
         </MapView>
       </View>
     </View>
    );
-};
-
-const style = {
-  "version": 8,
-  "name": "Heatmap",
-  "sources": {
-    'earthquakes': {
-      'type': 'geojson',
-      'data': 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson'
-    }
-  },
-  "center": [
-    74.24426803763072,
-    -2.2507114487818853
-  ],
-  "zoom": 0.6851443156248076,
-  "layers": [
-    {
-      'id': 'earthquakes-point',
-      'type': 'circle',
-      'source': 'earthquakes',
-      'minzoom': 7,
-      'paint': {
-          // Size circle radius by earthquake magnitude and zoom level
-          'circle-radius': [
-              'interpolate',
-              ['linear'],
-              ['zoom'],
-              7,
-              ['interpolate', ['linear'], ['get', 'mag'], 1, 1, 6, 4],
-              16,
-              ['interpolate', ['linear'], ['get', 'mag'], 1, 5, 6, 50]
-          ],
-          // Color circle by earthquake magnitude
-          'circle-color': [
-              'interpolate',
-              ['linear'],
-              ['get', 'mag'],
-              1,
-              'rgba(33,102,172,0)',
-              2,
-              'rgb(103,169,207)',
-              3,
-              'rgb(209,229,240)',
-              4,
-              'rgb(253,219,199)',
-              5,
-              'rgb(239,138,98)',
-              6,
-              'rgb(178,24,43)'
-          ],
-          'circle-stroke-color': 'white',
-          'circle-stroke-width': 1,
-          // Transition from heatmap to circle layer by zoom level
-          'circle-opacity': [
-              'interpolate',
-              ['linear'],
-              ['zoom'],
-              7,
-              0,
-              8,
-              1
-          ]
-      }
-    },
-    {
-      'id': 'earthquakes-heat',
-      'type': 'heatmap',
-      'source': 'earthquakes',
-      'maxzoom': 9,
-      'paint': {
-          // Increase the heatmap weight based on frequency and property magnitude
-          'heatmap-weight': [
-              'interpolate',
-              ['linear'],
-              ['get', 'mag'],
-              0,
-              0,
-              6,
-              1
-          ],
-          // Increase the heatmap color weight weight by zoom level
-          // heatmap-intensity is a multiplier on top of heatmap-weight
-          'heatmap-intensity': [
-              'interpolate',
-              ['linear'],
-              ['zoom'],
-              0,
-              1,
-              9,
-              3
-          ],
-          // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
-          // Begin color ramp at 0-stop with a 0-transparancy color
-          // to create a blur-like effect.
-          'heatmap-color': [
-              'interpolate',
-              ['linear'],
-              ['heatmap-density'],
-              0,
-              'rgba(33,102,172,0)',
-              0.2,
-              'rgb(103,169,207)',
-              0.4,
-              'rgb(209,229,240)',
-              0.6,
-              'rgb(253,219,199)',
-              0.8,
-              'rgb(239,138,98)',
-              1,
-              'rgb(178,24,43)'
-          ],
-          // Adjust the heatmap radius by zoom level
-          'heatmap-radius': [
-              'interpolate',
-              ['linear'],
-              ['zoom'],
-              0,
-              2,
-              9,
-              20
-          ],
-          // Transition from heatmap to circle layer by zoom level
-          'heatmap-opacity': [
-              'interpolate',
-              ['linear'],
-              ['zoom'],
-              7,
-              1,
-              9,
-              0
-          ]
-      }
-    },
-  ]
 };
