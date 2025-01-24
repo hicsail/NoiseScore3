b6fbfc9dab35ee67c87c2e63ec88f2e07fdfd763
cbolles <collinbolles@gmail.com>
2025-01-24 15:25:51 -0500

    Leverage expo av

--- packages/mobile/app/components/measurement/RecordControls.component.tsx
+++ packages/mobile/app/components/measurement/RecordControls.component.tsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Button } from 'react-native-paper';
 
-export default function RecordControls() {
+export interface RecordControlsProps {
+  startRecording: () => void;
+  stopRecording: () => void;
+}
+
+export default function RecordControls(props: RecordControlsProps) {
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+
+  const handlePress = () => {
+    if (isRecording) {
+      props.stopRecording();
+    } else {
+      props.startRecording();
+    }
+    setIsRecording(!isRecording);
+  };
+
   return (
     <View style={styles.container}>
       <Button mode='contained' icon='delete'>Clear</Button>
-      <Button mode='contained' icon='stop'>Stop</Button>
+      <Button mode='contained' icon='stop' onPress={handlePress}>{isRecording ? 'Stop' : 'Start'}</Button>
       <Button mode='contained' icon='arrow-right-bold'>Next</Button>
     </View>
   );
--- packages/mobile/app/pages/Measure.page.tsx
+++ packages/mobile/app/pages/Measure.page.tsx
@@ -3,14 +3,60 @@ import { Text } from 'react-native-paper';
 import DecibelChart from '../components/measurement/DecibelChart.component';
 import DecibelSummary from '../components/measurement/DecibelSummary.component';
 import RecordControls from '../components/measurement/RecordControls.component';
+import { Audio } from 'expo-av';
+import { useState } from 'react';
+import { Recording } from 'expo-av/build/Audio';
 
 export default function Measure() {
+
+  const [recording, setRecording] = useState<Recording | undefined>(undefined);
+  const [permissionResponse, requestPermission] = Audio.usePermissions();
+
+  async function startRecording() {
+
+    try {
+      if (permissionResponse && permissionResponse.status !== 'granted') {
+        console.log('Requesting permission..');
+        await requestPermission();
+      }
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync( Audio.RecordingOptionsPresets.HIGH_QUALITY, (status) => console.log(status));
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    if (!recording) {
+      console.error('Recording undefined');
+      return;
+    }
+
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    await Audio.setAudioModeAsync(
+      {
+        allowsRecordingIOS: false,
+      }
+    );
+    const uri = recording.getURI();
+    console.log('Recording stopped and stored at', uri);
+  }
+
   return (
     <View>
       <Text variant='headlineLarge' style={styles.statusText}>Stopped</Text>
       <DecibelChart />
       <DecibelSummary readings={[20, 50, 100]}/>
-      <RecordControls />
+      <RecordControls startRecording={startRecording} stopRecording={stopRecording}/>
     </View>
   );
 }
